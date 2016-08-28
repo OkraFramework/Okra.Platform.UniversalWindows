8780cc3d75a45cd49a0a746e13a2f8a0b39f0f57
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2016-08-28 20:04:19 +0100

    Update to UWP 5.2.2

--- src/Okra.Platform.UniversalWindows/Mvvm/ViewPresenter.cs
+++ src/Okra.Platform.UniversalWindows/Mvvm/ViewPresenter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Okra.Routing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,26 +12,26 @@ namespace Okra.Mvvm
     public class ViewPresenter : Frame
     {
         public static readonly DependencyProperty CurrentViewProperty =
-            DependencyProperty.Register("CurrentView", typeof(object), typeof(ViewPresenter), new PropertyMetadata(null, CurrentViewChanged));
+            DependencyProperty.Register("CurrentView", typeof(ViewInfo), typeof(ViewPresenter), new PropertyMetadata(null, CurrentViewChanged));
         
         public ViewPresenter()
         {
         }
 
-        public object CurrentView
+        public ViewInfo CurrentView
         {
-            get { return (object)GetValue(CurrentViewProperty); }
+            get { return (ViewInfo)GetValue(CurrentViewProperty); }
             set { SetValue(CurrentViewProperty, value); }
         }
 
         private static void CurrentViewChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            ((ViewPresenter)d).CurrentViewChanged(e.OldValue, e.NewValue);
+            ((ViewPresenter)d).CurrentViewChanged((ViewInfo)e.OldValue, (ViewInfo)e.NewValue);
         }
 
-        public void CurrentViewChanged(object oldView, object newView)
+        public void CurrentViewChanged(ViewInfo oldViewInfo, ViewInfo newViewInfo)
         {
-            this.Navigate(typeof(ViewHost), newView);
+            this.Navigate(typeof(ViewHost), newViewInfo.View);
         }
     }
 }
